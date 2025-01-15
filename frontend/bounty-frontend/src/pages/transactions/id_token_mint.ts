8f10e37463543aa6b7e1e34719d3f602c87b3f22
Ken Lau <kenlauhong2015@gmail.com>
2025-01-15 16:17:09 +0800

    added github para

--- frontend/bounty-frontend/src/pages/transactions/id_token_mint.ts
+++ frontend/bounty-frontend/src/pages/transactions/id_token_mint.ts
@@ -15,7 +15,7 @@ import {
 } from "@meshsdk/core";
 import { getUtxoApiRoute, updateUtxoApiRoute } from "./api_common";
 
-export const mintIdToken = async (github: string, wallet: IWallet) => {
+export const mintIdToken = async (gitHub: string, wallet: IWallet) => {
   if (!wallet) {
     alert("Please connect your wallet");
     return;
@@ -195,7 +195,7 @@ export const mintIdToken = async (github: string, wallet: IWallet) => {
           constructor: 0,
           fields: [
             {
-              bytes: stringToHex(github),
+              bytes: stringToHex(gitHub),
             },
             { list: [] },
           ],
