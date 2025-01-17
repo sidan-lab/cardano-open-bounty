8875f9be035498e5997e38ce00d705c32c5b22f3
Gary <hsiehhungluiyee0422@gmail.com>
2025-01-17 18:08:28 +0800

    Updated bounty board

--- frontend/bounty-frontend/src/components/CreateIDToken.tsx
+++ frontend/bounty-frontend/src/components/CreateIDToken.tsx
@@ -76,13 +76,13 @@ const CreateIDToken: React.FC = () => {
             </p>
             <div className="flex justify-center space-x-4 mt-4">
               <button
-                className="text-white bg-green-600 hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 transition duration-200 ease-in-out"
+                className="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 transition duration-200 ease-in-out"
                 onClick={() => setFormVisible(true)}
               >
                 Yes
               </button>
               <button
-                className="text-white bg-red-600 hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 transition duration-200 ease-in-out"
+                className="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 transition duration-200 ease-in-out"
                 onClick={() => setShowMintPrompt(false)}
               >
                 No
